321c57ef4ca527abc3e3ac4276340fa12d61e36a
Manasi <manasipatro3@gmail.com>
2025-02-18 13:21:00 +0530

    book

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -13,9 +13,8 @@ function Freebook() {
     const getBook = async () => {
       try {
         const res = await axios.get("http://localhost:4001/book");
-
+        console.log(res.data);
         const data = res.data.filter((data) => data.category === "Free");
-        console.log(data);
         setBook(data);
       } catch (error) {
         console.log(error);
